bfac7468e6858bc2ea7bcffa84ce4318a73e2902
nohjuhyeon <njh2720@gmail.com>
2024-04-12 09:17:17 +0000

    first commit

---
+++ src/main/java/com/example/co_templates/quests/restapi/BoardController.java
@@ -0,0 +1,72 @@
+package com.example.co_templates.quests.restapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.co_templates.quests.services.BoardService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+public class BoardController {
+
+    @Autowired BoardService BoardService;
+
+    @GetMapping("q/r/board/callDao")
+    public ResponseEntity<Object> callDao(HashMap<String, Object> dataMap){
+        HashMap<String, Object> resultMap = new HashMap<>();
+        resultMap =  BoardService.dao_mixed(dataMap);
+        return ResponseEntity.ok().body(resultMap);
+    }
+    @GetMapping("/q/r/board/mixed/{pageNumber}/{pk_id}")
+    public ResponseEntity<HashMap<String, Object>> mixed(@PathVariable("pageNumber") String pageNumber,
+            @PathVariable("pk_id") String pkId) {
+        // call service
+        HashMap<String, Object> resultMap = new HashMap<>();
+        resultMap = BoardService.mixed(pageNumber, pkId);
+
+        // add request params 
+        HashMap<String, Object> requestParams = new HashMap<>();
+        requestParams.put("pageNumber", pageNumber);
+        requestParams.put("pk_id", pkId);
+        resultMap.put("requestParams", requestParams);
+
+        return ResponseEntity.ok().body(resultMap);
+    }
+
+    @GetMapping("/q/r/board/list/{pageNumber}")
+    public ResponseEntity<List<HashMap<String, Object>>> list(@PathVariable("pageNumber") String pageNumber) {
+        ArrayList<HashMap<String, Object>> itemList = new ArrayList<HashMap<String, Object>>();
+        // CommonCodeService commonCodeService = new CommonCodeService();
+        itemList = BoardService.list(pageNumber);
+    
+        return ResponseEntity.ok().body(itemList);
+    }
+    @GetMapping("/q/r/board/insert/{title}/{contents}")
+    public ResponseEntity<Integer> insert(@PathVariable("title") String title, @PathVariable("contents") String contents) {
+        ArrayList<HashMap<String, Object>> itemList = new ArrayList<HashMap<String, Object>>();
+        // CommonCodeService commonCodeService = new CommonCodeService();
+        itemList = BoardService.insert(title,contents);
+    
+        return ResponseEntity.ok().body(itemList.size());
+    }
+    @GetMapping("q/r/board/view/{pk_id}")
+    public ResponseEntity<HashMap<String, Object>> view(@PathVariable("pk_id") String pkId) {
+        HashMap<String, Object> itemDetails = new HashMap<>();
+        itemDetails = BoardService.view(pkId);
+
+        return ResponseEntity.ok().body(itemDetails);
+    }
+    @GetMapping("q/r/board/delete/{pk_id}")
+    public ResponseEntity<Integer> delete(@PathVariable("pk_id") String pkId) {
+        int delete_num = BoardService.delete(pkId);
+
+        return ResponseEntity.ok().body(delete_num);
+    }
+}
---
+++ src/main/java/com/example/co_templates/services/CommonCodeService.java
@@ -0,0 +1,87 @@
+package com.example.co_templates.services;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.co_templates.daos.ShareDao;
+import com.example.co_templates.utils.Commons;
+
+@Service
+public class CommonCodeService {
+    @Autowired
+    ShareDao shareDao;
+
+    @Autowired
+    Commons commons;
+    public void callDao(HashMap<String, Object> dataMap){
+        // 여러개 가져오기 
+        String sqlMapId = "CommonCode.selectBysearch";
+        Object list = shareDao.getList(sqlMapId, dataMap);
+        
+        sqlMapId = "CommonCode.selectByUID";
+        dataMap.put("PK_UNIQUE", "CC002");
+        Object one = shareDao.getOne(sqlMapId, dataMap);
+        
+        sqlMapId = "CommonCode.insert";
+		// INSERT INTO `COMMON_CODE` 
+		// (`PK_UNIQUE`, `CODE_NAME`, `DESCRIPTION`, `PARENT_UNIQUE`) 
+		// VALUES ('CC017', 'test_insert', 'test insert', '');
+        String pk_unique_id = commons.getUniqueSequence();
+        dataMap.put("PK_UNIQUE",pk_unique_id);
+        dataMap.put("CODE_NAME","test_insert");
+        dataMap.put("DESCRIPTION","test insert");
+        dataMap.put("PARENT_UNIQUE","");
+        Object insert = shareDao.insert(sqlMapId, dataMap);
+
+        sqlMapId = "CommonCode.update";
+        dataMap.put("PK_UNIQUE",pk_unique_id);
+        dataMap.put("CODE_NAME","test_ROOT");
+        Object update = shareDao.update(sqlMapId, dataMap);
+
+        sqlMapId = "CommonCode.delete";
+        dataMap.put("PK_UNIQUE",pk_unique_id);
+        Object delete = shareDao.delete(sqlMapId, dataMap);
+
+        return;
+    }
+
+    public HashMap<String, Object> mixed(Integer pageNumber, Integer pkid){
+        HashMap<String, Object> resultMap = new HashMap<>();
+        resultMap.put("list", this.list(pageNumber));
+        resultMap.put("view", this.view(pkid));
+        return resultMap;
+    }
+
+    public ArrayList<HashMap<String, Object>> list(Integer pageNumber){
+        ArrayList<HashMap<String, Object>> itemList = new ArrayList<HashMap<String, Object>>();
+ 
+        HashMap<String, Object> item = new HashMap<>();
+        item.put("PK_ID", 1);
+        item.put("FK_ID", 10);
+        item.put("NAME", "Excalibur");
+        itemList.add(item);
+    
+        item = new HashMap<>();
+        item.put("PK_ID", 2);
+        item.put("FK_ID", 20);
+        item.put("NAME", "Dragon Sword");
+        itemList.add(item);
+
+        return itemList;
+    }
+
+    public HashMap<String, Object> view(Integer pkId){
+        HashMap<String, Object> itemDetails = new HashMap<>();
+
+        String fkId = "FK_0382";
+        String name = "Commons";
+        itemDetails.put("PK_ID", pkId);
+        itemDetails.put("FK_ID", fkId);
+        itemDetails.put("NAME", name);
+
+        return itemDetails;
+    }
+}
