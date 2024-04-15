ab9bc9fd9034c007b1f55d5b0512af66144b9e6f
nohjuhyeon <njh2720@gmail.com>
2024-04-15 08:40:36 +0000

    controller

--- src/main/java/com/example/co_templates/controllers/TemplateJSP.java
+++ src/main/java/com/example/co_templates/controllers/TemplateJSP.java
@@ -4,15 +4,55 @@ package com.example.co_templates.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import java.util.HashMap;
 
 @Controller
 public class TemplateJSP {
+    ModelAndView modelAndView = new ModelAndView();
+
     @GetMapping("/templateWithJSP")
     public ModelAndView templateWithJSP(){
-        String viewPath = "src/webapp/WEB-INF/views/template.jsp";
-        ModelAndView ModelAndView = new ModelAndView();
-        ModelAndView.setViewName(viewPath);
-        return ModelAndView;
+        String viewPath = "/WEB-INF/views/template.jsp";
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName(viewPath);
+        return modelAndView;
+    }
+
+    @GetMapping("/templateWithJSPstatements")
+    public ModelAndView jspstatements(ModelAndView modelAndView ){
+        modelAndView.addObject("name","cocolang");
+        modelAndView.addObject("age","1");
+
+        String viewPath = "/WEB-INF/views/template_jspstatements.jsp";
+        modelAndView.setViewName(viewPath);
+        return modelAndView;
+    }
+
+    @GetMapping("/templateWithServletStatements")
+    public String templateWithServletStatements(){
+        HashMap<String, Object> model = new HashMap<>();
+        model.put("name", "cocolang");
+        model.put("age", "1");
+        int day = 3;
+
+        String htmlAll = "<html>";
+        htmlAll = htmlAll + "<head><title>IF...ELSE Example</title></head>";
+        htmlAll = htmlAll + "<body>";
+        if (day == 1 || day == 7) { 
+            htmlAll = htmlAll + "<p> Today is weekend</p>";
+         } else {
+            htmlAll = htmlAll + "<p> Today is not weekend</p>";            
+        }         
+        htmlAll = htmlAll + "</body>";
+        htmlAll = htmlAll + "</html>";
+
+        htmlAll = htmlAll + "<div>";
+        String name = (String) model.get("name");
+        htmlAll = htmlAll +"<div>name : " + name + "</div>";
+        String age = (String) model.get("age");
+        htmlAll = htmlAll +"<div>age : " + age + "</div>";
+        htmlAll = htmlAll +"</div>";
+        return htmlAll ;
     }
     
 }
